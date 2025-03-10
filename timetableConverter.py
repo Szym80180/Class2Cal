ff6421fd206e812d96d37d6559b00589c90742fa
Szym80180 <szym80180@gmail.com>
2025-03-10 23:18:28 +0100

    Implemented the reading of the file. Extracting table cells

--- timetableConverter.py
+++ timetableConverter.py
@@ -1,18 +1,51 @@
 import re
 from bs4 import BeautifulSoup as bs
 from bs4 import SoupStrainer
-
+import bs4
 FILENAME = "sis.html"
 
 only_table = SoupStrainer(id="tb")
 soup = bs(open(FILENAME, encoding="utf-8"), "html.parser", parse_only=only_table)
 
-cells = soup.find_all("tr")
-for cell in cells:
-    for child in cell.children:
-        print(child)
+timetable = {
+    "0": [],
+    "1": [],
+    "2": [],
+    "3": [],
+    "4": []
+}
 
-
+event = {
+    "time": "",
+    "name": "",
+    "room": "",
+    "type": ""
+}
+time = 0
+day=0
+i=0
+rows = soup.find_all("tr")
+for row in rows:
+    i=0
+    for cell in row.find_all("td"):
+        if i == 0:
+            time = cell.get_text(strip=True)
+            i += 1
+        else:
+            if cell.get_text(strip=True) == "":
+                print("Empty cell")
+                day+=1
+                continue
+            else:
+                for tag in cell.find_all("br"):
+                    tag.replace_with(" ")
+                for b_tag in cell.find_all("b"):
+                    b_tag.unwrap()
+                    
+                for tag in cell:
+                    print(tag)
+               
+       
 
 
 
