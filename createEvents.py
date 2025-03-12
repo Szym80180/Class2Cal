9ab17596b8e42176100d7c26a64857dfbeb62b14
Szym80180 <szym80180@gmail.com>
2025-03-12 09:35:28 +0100

    Now adding events to timetable. Everything is added correctly

--- createEvents.py
+++ createEvents.py
@@ -1,7 +1,24 @@
 import re
+from prettytable import PrettyTable
 
 INPUT_NAME = "parsed.txt"
 
+def next_day(day):
+    if day+1 ==5:
+        return 0
+    return day+1
+
+def insert(day, event):
+    if day ==0:
+        timetable["monday"].append(event)
+    elif day ==1:
+        timetable["tuesday"].append(event)
+    elif day ==2:
+        timetable["wednesday"].append(event)
+    elif day ==3:
+        timetable["thursday"].append(event)
+    elif day ==4:
+        timetable["friday"].append(event)
 
 re_hour = r"(\d{2}:\d{2})"
 re_grA = r"gr.A"
@@ -19,20 +36,47 @@ time=0
 
 event = {}
 events=[]
-
+timetable={
+    "monday": [],
+    "tuesday": [],
+    "wednesday": [],
+    "thursday": [],
+    "friday": []
+}
+day=0
+prev_is_time=False
 for line in lines:
+    
     if "\n" in line:
         line = line.replace("\n", "")
-    if line == "pusty":
+    #print(line)
+    if line == "dzien":
+        if iterator ==4:
+            events.append(event)
+            #print(event) 
+        if not prev_is_time:
+            day = next_day(day)
+            #print(f"dzien, przestawiony dzine na {day}")
         iterator = 0
         input_event = False
         continue
+    prev_is_time=False
+    # if line == "pusty":
+    #     day = next_day(day)
+    #     print(f"pusty, przestawiony dzine na {day}")
+    #     iterator = 0
+    #     input_event = False
+    #     continue
+    
     if re.match(re_hour, line):
         time = line
+        day=0
+        prev_is_time=True
         continue
     
     if iterator == 0:
         event={}
+        event["day"] = day
         event["time"] = time
         event["room"] = line
         iterator += 1
@@ -65,13 +109,10 @@ for line in lines:
             newevent=False
         if newevent:
             events.append(event)
-            print(event)
-            event={}
-            event["time"] = time
-            event["room"] = line
-            iterator=1
-        else:
-            iterator=0
-            events.append(event)
-            print (event)
+            #print (event)
+    
+#print(events)
+for event in events:
+    insert(event["day"], event)
+
 
