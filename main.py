d8aac514afbfbf3530c1ae406934363e8c5eba3b
Szym80180 <szym80180@gmail.com>
2025-03-13 21:29:15 +0100

    Added messeges for user. Added taking the time of start and end of the semester from user input.

--- main.py
+++ main.py
@@ -50,28 +50,46 @@ def chooseColors():
     colors['[P]'] = chooseColor()
     return colors
 
-def getDate(event):
-        date = input(f"Data rozpoczęcie zajęć {event['name']} DLA CIEBIE - od tej daty zajecia beda sie pojawiac co dwa tygodnie:")
+def getDate():
+       
+        date = input( "KONIECZNIE WPISZ DATE W FORMACIE DDMMYYYY, inaczej program nie zadziala: ")
         if len(date) != 8 or not date.isdigit():
             print("Podano niepoprawną datę. Spróbuj ponownie.")
-            return getDate(event)
+            return getDate()
         return date
 
 
 
 def main():
-    start_date=datetime.datetime(2025,3,17)
+    print("Witaj w programie do automatycznego dodawania zajęć do kalendarza Google!")
+    print("Najpierw musisz się zalogować do swojego konta Google.")  
+    service = ch.createService()  
+    if not service:
+        print("Failed to create service. Please check your credentials and try again.")
+    print("Pomyślnie zalogowano do konta Google.")
+
+    print("Podaj najbliższy PONIEDZIAŁEK od daty rozpoczęcia zajęć - od tej daty pojawą się zajęcia w kalendarzu")
+    start_date=getDate()
+    try:
+        start_date = datetime.datetime.strptime(start_date, "%d%m%Y")
+    except ValueError:
+        print("Podano niepoprawną datę. Spróbuj ponownie.")
+        return
     current_date = start_date
-    def_until=datetime.datetime(2025, 6, 8, 23, 59, 59)
+
+    print("Podaj datę zakończenia zajęć - do tej daty pojawiają się zajęcia w kalendarzu")
+    end_date=getDate()
+    try:
+        def_until=datetime.datetime.strptime(end_date, "%d%m%Y")+datetime.timedelta(hours=23, minutes=59, seconds=59)
+    except ValueError:
+        print("Podano niepoprawną datę. Spróbuj ponownie.")
+        return
+   
     def_interval=1
     
     gr = getGroup()
     colors=chooseColors()
-
-
-    service = ch.createService()
-    if not service:
-        print("Failed to create service. Please check your credentials and try again.")
+    
     calendarId = ch.createCalendar(service)
     
     timetable = getEvents()
@@ -85,7 +103,6 @@ def main():
             interval = def_interval
 
             if("group" in event and event["group"] != gr):
-                print(f"Skipping event with group {event['group']}")
                 continue
 
             hour = (int(event["time"][0:2]))
@@ -104,8 +121,8 @@ def main():
                 
             if 'twoweeks' in event:
                 interval=2
-                print("KONIECZNIE WPISZ DATE W FORMACIE DDMMYYYY, inaczej program nie zadziala")
-                date = getDate(event)
+                print(f"Data rozpoczęcie zajęć {event['name']} DLA CIEBIE - od tej daty zajecia beda sie pojawiac co dwa tygodnie:")
+                date = getDate()
                 startdate = datetime.datetime.strptime(date, "%d%m%Y")
                 start = startdate.replace(hour=hour, minute=0, second=0)
                 
@@ -115,7 +132,7 @@ def main():
             end = end.isoformat()
             pushed_event = ch.createEvent(service, f"{event['type']} {event['name']}", event["room"], f"{event['lecturer']}", start, end, [recurrence], colors[event['type']])
             ch.insertEvent(service, calendarId, pushed_event)
-            print(pushed_event)
+            #print(pushed_event)
 
         current_date =current_date+ datetime.timedelta(days=1)
         
