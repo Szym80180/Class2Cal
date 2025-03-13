6471e9f7158a2331a531afd1e2ea2df5c5ea97e9
Szym80180 <szym80180@gmail.com>
2025-03-13 22:26:59 +0100

    Fitted the code for importing into main.py

--- parseTimetable.py
+++ parseTimetable.py
@@ -8,33 +8,39 @@ import bs4
 FILENAME = "sis.html"
 OUTPUT_FILENAME = "parsed.txt"
 
-file = open(OUTPUT_FILENAME,"w", encoding = "utf-8") 
+def parseHTML():
+    try:
+        file = open(OUTPUT_FILENAME,"w", encoding = "utf-8") 
+    except FileNotFoundError:
+        print(f"Nie znaleziono pliku {FILENAME}")
+        exit(1)
 
-only_table = SoupStrainer(id="tb")
-soup = bs(open(FILENAME, encoding="utf-8"), "html.parser", parse_only=only_table)
+    only_table = SoupStrainer(id="tb")
+    soup = bs(open(FILENAME, encoding="utf-8"), "html.parser", parse_only=only_table)
 
 
-rows = soup.find_all("tr")
+    rows = soup.find_all("tr")
 
-for row in rows:
-    i=0
-    for cell in row.find_all("td"):
-        file.write("dzien\n")
-        if i == 0:
-            time = cell.get_text(strip=True)
-            file.write(f"{time}\n")
-            i += 1
-            continue
-        if cell.get_text(strip=True)=="":
-            file.write("pusty\n")
-        else:
-            for tag in cell:
-                if tag.name == "br":
-                    pass
-                elif tag.get_text(strip=True)=="":
-                    pass
-                else:
-                    file.write(f"{tag.get_text()}\n")
-                    #print(tag.name)
+    for row in rows:
+        i=0
+        for cell in row.find_all("td"):
+            file.write("dzien\n")
+            if i == 0:
+                time = cell.get_text(strip=True)
+                file.write(f"{time}\n")
+                i += 1
+                continue
+            if cell.get_text(strip=True)=="":
+                file.write("pusty\n")
+            else:
+                for tag in cell:
+                    if tag.name == "br":
+                        pass
+                    elif tag.get_text(strip=True)=="":
+                        pass
+                    else:
+                        file.write(f"{tag.get_text()}\n")
+                        #print(tag.name)
 
-file.close()
+    file.close()
+    return
